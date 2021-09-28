b54b5f84bcdbe44722c84b13337933c4ef1c3aad
amsakanna <amsakanna@gmail.com>
2021-09-28 16:59:14 +0530

    Enhancement: Composer function names are bad Fixes #14

--- lib/src/composer/composer.dart
+++ lib/src/composer/composer.dart
@@ -1,22 +1,25 @@
+import 'package:zam_core/core.dart';
+
 import '../application/application.dart';
 import 'composer.config.dart';
 
 ///
 /// Utility class to compose modules, build and run app.
 ///
-abstract class Composer {
+abstract class Composer implements AsyncRunnable {
   ComposerConfig get config;
 
   ///
   /// Use this if you want to run the app manually.
   ///
-  Application buildApp();
+  Application build();
 
   ///
   /// Builds and runs the app
   ///
-  Future<Object?> runApp() async {
-    final application = buildApp();
+  @override
+  Future<Object?> run() async {
+    final application = build();
     await application.initialize();
     return application.run();
   }
